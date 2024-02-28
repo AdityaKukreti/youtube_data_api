349389e4390af62def92968c770bfb482aba1469
AdityaKukreti <kukreti.adi@gmail.com>
2024-02-28 21:49:38 +0530

    commit

--- youtubeAPI.py
+++ youtubeAPI.py
@@ -29,7 +29,7 @@ class YoutubeAPI:
         return statistics_response
     def search(self):
         request = self.youtube.search().list(
-            q = self.query + ' education ' + 'tutorial ' + 'lecture',
+            q = self.query + "Tutorial Introduction Basics Fundamentals Explained Principles Key concepts Training Lecture Workshop Educational",
             part = 'snippet',
             maxResults = 20,
             type='video',
