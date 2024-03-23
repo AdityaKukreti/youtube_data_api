b18af413b41ff3679f60e5bbfbfbd7c738dd6bcb
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2024-03-23 18:39:58 +0530

    Update youtubeAPI.py

--- youtubeAPI.py
+++ youtubeAPI.py
@@ -41,7 +41,8 @@ class YoutubeAPI:
            
             content = {}
             # if (i['id']['kind'] == "youtube#video"):
-            print(str(i['snippet']) + '\n\n\n')
+            print(str(i['snippet']['title']) + '\n\n\n')
+            print(str(i['snippet']['description']) + '\n\n\n')
             content['id'] = i['id']
             content['snippet'] = i['snippet']
             content['items'] = self.statistics(i['id']['videoId'])['items']
