216fdfae2d51f787e358f9e6ce4b6444376d1f85
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2024-04-03 12:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from youtubeAPI import *
 from notesAPI import *
 from flask import Flask,jsonify,request
 
+UPLOAD_FOLDER = 'uploads/'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 youtubeAPI = YoutubeAPI()
 notesAPI = NotesGenerator()
 
@@ -39,7 +42,22 @@ def generateQuiz():
         return jsonify({'status':'true','data':notesAPI.generateQuiz(videoId)})
     except:
         return jsonify({'status':'false'})
+    
 
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No file selected', 400
+
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+
+    return 'File uploaded successfully', 200
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
